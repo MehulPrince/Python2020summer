10dcfbd3862cda995e85616b23dc1da6f79b4b56
MehulPrince <mhulprnce@gmail.com>
2020-04-02 11:53:54 +0530

    constructors

---
+++ constructors.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi,my name is {self.name}")
+
+
+john = Person("Bob Smith")
+john.talk()
